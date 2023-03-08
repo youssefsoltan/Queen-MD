1c9aa6778364ee3e77138de866b984d1a49f2e56
youssefsoltan <97458904+youssefsoltan@users.noreply.github.com>
2023-03-09 01:40:03 +0200

    Update config.js

--- config.js
+++ config.js
@@ -22,17 +22,17 @@ global.APIKeys = {
 ////////////////////////////////////////////////////|:|             OWNER DETAILS             |:|//////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 global.session = process.env.SESSION_ID || 'PUT HERE',
-global.ownername = process.env.OWNER_NAME === undefined ? "DarkWinzo" : process.env.OWNER_NAME
-let fake = process.env.OWNER_NUMBER === undefined ? '94775200935' : process.env.OWNER_NUMBER
+global.ownername = process.env.OWNER_NAME === undefined ? "YoUsSef Al SoLtAn" : process.env.OWNER_NAME
+let fake = process.env.OWNER_NUMBER === undefined ? '201227179105' : process.env.OWNER_NUMBER
 global.owner = [`${fake}`]
 global.ownernumber = [`${fake}`]
 global.premium = [`${fake}`]
-global.anticall = process.env.ANTICALL === undefined ? 'false' : process.env.ANTICALL
-global.packname = process.env.PACK_NAME === undefined ? 'Queen-MD' : process.env.PACK_NAME
-global.author = process.env.STICKER_AUTHOR_NAME === undefined ? 'DarkWinzo' : process.env.STICKER_AUTHOR_NAME
+global.anticall = process.env.ANTICALL === undefined ? 'true' : process.env.ANTICALL
+global.packname = process.env.PACK_NAME === undefined ? 'BOT - AL SOLTAN' : process.env.PACK_NAME
+global.author = process.env.STICKER_AUTHOR_NAME === undefined ? '201227179105' : process.env.STICKER_AUTHOR_NAME
 global.pmblock = process.env.PM_BLOCK === undefined ? 'false' : process.env.PM_BLOCK
-global.chatbot_pm = process.env.PM_CHATBOT === undefined ? 'false' : process.env.PM_CHATBOT
-global.chatbot_group = process.env.GROUP_CHATBOT === undefined ? 'false' : process.env.GROUP_CHATBOT
+global.chatbot_pm = process.env.PM_CHATBOT === undefined ? 'true' : process.env.PM_CHATBOT
+global.chatbot_group = process.env.GROUP_CHATBOT === undefined ? 'true' : process.env.GROUP_CHATBOT
 
 /////////////////////////////////////////////////////////////////||SESSION ID METHOD||///////////////////////////////////////////////////////////////////////
 global.sessionconvert = ['true']                           //This is session id method.....//Don't Change
